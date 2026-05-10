fd2dcde520d7adfa1ad7e33f760912149aa0e44f
hakim <125070824+anynonenom@users.noreply.github.com>
2026-05-10 03:34:10 +0100

    use django.views.static.serve for media in production

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
+import os
+
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from apps.accounts.views import health_check
 from apps.approvals.views import org_approval_queue
@@ -37,4 +39,6 @@ urlpatterns = [
     path("", include("apps.accounts.urls_root")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
